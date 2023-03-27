db68b5b14f41f30caefcc253a2804425f623d523
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-27 10:46:36 -0400

    Update data structure

--- data/scholarship.js
+++ data/scholarship.js
@@ -1,14 +1,32 @@
-const scholarships = [
-    {
-        "Name":"scholarship1",
-        "value": "5,000",
-        "deadline": "Dec 21, 2024"
-    },
-    {
-        "Name":"scholarship2",
-        "value": "6,000",
-        "deadline": "Dec 1, 2023"
-    }
-]
+const scholarships = {
+    "data": [
+        {
+            "id": "1",
+            "type": "scholarship",
+            "attributes": {
+                "title": "DEF CON 31",
+                "organization": "Women in Security and Privacy (WISP)",
+                "amount": "1234",
+                "content": "Lorem ipsum dolor sit amet, ...",
+                "deadline": "March 01, 2023",
+                "URL": "https://linkedin.com/...",
+                "image_url": "https://shutter_stock_static_image"
+            }
+        },
+        {
+            "id": "2",
+            "type": "scholarship",
+            "attributes": {
+                "title": "RailsConf 23",
+                "organization": "Ruby Central",
+                "amount": "1234",
+                "content": "Lorem ipsum dolor sit amet, ...",
+                "deadline": "March 01, 2023",
+                "URL": "https://linkedin.com/...",
+                "image_url": "https://static_image.wow_nice"
+            }
+        }
+    ]
+}
 
 module.exports = scholarships
